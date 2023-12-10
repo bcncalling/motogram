09f2e93353e578a8e4456047feee0acf0b316dcc
bcncalling <142149038+bcncalling@users.noreply.github.com>
2023-12-10 18:03:16 +0000

    Update __init__.py

--- motogram/scraps/__init__.py
+++ motogram/scraps/__init__.py
@@ -1,4 +1,18 @@
 from ..save_session import SaveSession
-from ..go_start import go, Start
+from ..go_start import Go, Start
 from ..restart_stop import Restart, Stop
 from ..stop_trans import StopTrans
+from ..handlers import AddHandler, RemoveHandler
+
+class scraps (
+    SaveSession,
+    Go,
+    Start,
+    Restart,
+    Stop,
+    StopTrans,
+    AddHandler,
+    RemoveHandler
+):
+    pass
+  
