4920d1e3abc3700b38c19097d8c2df8d18697d7c
bcncalling <142149038+bcncalling@users.noreply.github.com>
2023-12-10 18:50:16 +0000

    Update go_start.py

--- motogram/scraps/go_start.py
+++ motogram/scraps/go_start.py
@@ -1,22 +1,21 @@
-"""
-motogram: An asynchronous Python wrapper for the Telegram Bot API.
+#motogram: An asynchronous Python wrapper for the Telegram Bot API.
+#
+#This library is based on pyrogram (https://github.com/pyrogram/pyrogram).
+#Copyright (C) 2023 [Santhu]
+#
+#motogram is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
 
-This library is based on pyrogram (https://github.com/pyrogram/pyrogram).
-Copyright (C) 2023 [Santhu]
+#motogram is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#
+#You should have received a copy of the GNU General Public License
+#along with motogram.  If not, see <https://www.gnu.org/licenses/>.
 
-motogram is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-motogram is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with motogram.  If not, see <https://www.gnu.org/licenses/>.
-"""
 
 import asyncio
 import inspect
